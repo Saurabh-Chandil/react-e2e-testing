64b94dff5f0192efe30c5bedfa0fd63c33a3bbf4
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2022-03-16 18:41:43 +0530

    contains & should

--- cypress/integration/my-tests/hello-world.test.js
+++ cypress/integration/my-tests/hello-world.test.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
-describe("empty test", () => {
+describe("e2e test", () => {
     it('test one', () => {
-        throw new Error('error occurred')
+        // throw new Error('error occurred')
+        cy.visit('https://www.nodejs.org')
+        cy.contains('Download for Windows (x64')
+        // cy.contains('Node.js® is a JavaScript runtime built').should('exist')
+        
+        // cy.get('h2#home-downloadhead').contains('Download for Windows (x64)')
+        //cy.get('div#home-downloadhead') // not found
+
+        // cy.get('h2#home-downloadhead')
+        // .should('have.text', 'Download for Windows (x64)')
+        
+
     })
 })
